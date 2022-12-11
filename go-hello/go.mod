fbb85dbc691c32a8a3c8d1df5cbff3933907816a
deividmarreiro <davidteixeiro@gmail.com>
2022-12-11 01:35:53 -0300

    Go mod initialized and Hello Test Create

---
+++ go-hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/deividmarreiro/Learn-Go-with-Tests/go-hello
+
+go 1.19
---
+++ go-hello/hello_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	result := Hello()
+	expected := "Hello, World"
+
+	if result != expected {
+		t.Errorf("result '%s', expected '%s'", result, expected)
+	}
+}
